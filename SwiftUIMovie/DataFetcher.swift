542698af62dcc027f96d423b80a0604c8ade73da
Saurabh Jaiswal <saurabh@astrosage.com>
2025-12-24 17:22:21 +0530

    Created helper func to build url paths.

--- SwiftUIMovie/DataFetcher.swift
+++ SwiftUIMovie/DataFetcher.swift
@@ -11,26 +11,15 @@ struct DataFetcher {
     let tmdbBaseURL = APIConfig.shared?.tmdbBaseURL
     let tmdbAPIKey = APIConfig.shared?.tmdbAPIKey
     
-    func fetchTitles(for media: String) async throws -> [Title] {
-        guard let baseURL = tmdbBaseURL else {
-            throw NetworkError.missingConfig
-        }
+    func fetchTitles(for media: String, by type: String) async throws -> [Title] {
+        let fetchTitlesURL = try buildURL(media: media, type: type)
         
-        guard let apiKey = tmdbAPIKey else {
-            throw NetworkError.missingConfig
-        }
-        
-        guard let fetchTitleURL = URL(string: baseURL)?
-            .appending(path: "3/trending/\(media)/day")
-            .appending(queryItems: [
-                URLQueryItem(name: "api_key", value: apiKey)
-            ]) else {
+        guard let fetchTitlesURL = fetchTitlesURL else {
             throw NetworkError.urlBuildFailed
         }
+        print(fetchTitlesURL)
         
-        print(fetchTitleURL)
-        
-        let(data, urlResponse) = try await URLSession.shared.data(from: fetchTitleURL)
+        let(data, urlResponse) = try await URLSession.shared.data(from: fetchTitlesURL)
         
         guard let response = urlResponse as? HTTPURLResponse, response.statusCode == 200 else {
             throw NetworkError.badURLResponse(underlyingError: NSError(
@@ -44,6 +33,36 @@ struct DataFetcher {
         var titles = try decoder.decode(APIObject.self, from: data).results
         Constants.addPosterPath(to: &titles)
         return titles
+    }
+    
+    private func buildURL(media: String, type:String) throws -> URL? {
+        guard let baseURL = tmdbBaseURL else {
+            throw NetworkError.missingConfig
+        }
+        
+        guard let apiKey = tmdbAPIKey else {
+            throw NetworkError.missingConfig
+        }
+        
+        var path: String
+        
+        if type == "trending" {
+            path = "3/trending/\(media)/day"
+        } else if type == "top_rated" {
+            path = "3/\(media)/top_rated"
+        } else {
+            throw NetworkError.urlBuildFailed
+        }
+        
+        guard let url = URL(string: baseURL)?
+            .appending(path: path)
+            .appending(queryItems: [
+                URLQueryItem(name: "api_key", value: apiKey)
+            ]) else {
+            throw NetworkError.urlBuildFailed
+        }
+        
+        return url
     }
 }
 
